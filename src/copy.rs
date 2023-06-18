e4569f2fce3f363fb45159b62d1a0d508be6db0c
Macbook <macbook@Macbooks-MacBook-Pro-3.local>
2023-06-18 21:04:30 +0500

    copy types added

---
+++ src/copy.rs
@@ -0,0 +1,16 @@
+//this page is for the copy types
+// its working bcz its copy
+fn print_value(number: i32) {
+    println!("{}", number);
+}
+
+fn main() {
+    let a: i32 = 32;
+    print_value(a);
+    print_value(a);
+}
+
+
+
+
+// now for the string has no copy but can do clone 
