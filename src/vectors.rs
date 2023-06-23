356602971d6c3c52d8968cca113b91c5296ad728
Macbook <macbook@Macbooks-MacBook-Pro-3.local>
2023-06-23 13:21:07 +0500

    vectors

--- src/vectors.rs
+++ src/vectors.rs
@@ -1,1 +1,14 @@
-fn main () {}
+use std::os::unix::prelude::PermissionsExt;
+
+fn main() {
+    let my_vec = vec![vec!["lahore", "karachi", "isb"], vec!["23", "43", "34"]];
+
+    for mut x in my_vec {
+        println!("city = {}", x[0]);
+        while let Some(something) = x.pop() {
+            if let Ok(number) = something.parse::<i32>() {
+                println!("The number is {}", number);
+            }
+        }
+    }
+}
